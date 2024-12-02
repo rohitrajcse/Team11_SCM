ca07140b71e3d53b364ede772ab0a4594b799ebd
himanshu.22scse1011498@galgotiasuniversity.edu.in <smart.himanshu02@gmail.com>
2024-12-02 14:03:22 +0530

    view controller added and background color changed by cocoa touch class

---
+++ Team11_SCM/BlueViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BlueViewController.swift
+//  Team11_SCM
+//
+//  Created by Batch - 2 on 02/12/24.
+//
+
+import UIKit
+
+class BlueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
